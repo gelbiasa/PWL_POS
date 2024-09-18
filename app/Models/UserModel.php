49590dcc3286c1294a5640f3aa5778a6935d7f64
gelbiasa <gelbifirmansyah12@gmail.com>
2024-09-18 10:07:29 +0700

    JS4 Praktikum 2.4

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -10,5 +10,5 @@ class UserModel extends Model
     use HasFactory;
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
-    protected $fillable = ['level_id', 'username', 'nama'];
+    protected $fillable = ['password', 'level_id', 'username', 'nama'];
 }
